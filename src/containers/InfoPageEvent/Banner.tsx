5e820e4f9956fe9e835ab939f67b18bd986a9829
aguiquiss03 <neinaldoaguinaldo13@gmail.com>
2024-06-24 18:02:30 +0100

    feat: reduzir as images

--- src/containers/InfoPageEvent/Banner.tsx
+++ src/containers/InfoPageEvent/Banner.tsx
@@ -6,7 +6,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import {
-  bannerImage,
   bannerIllustration,
   Banner3,
   Banner4,
