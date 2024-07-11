f81648b280ceb696b59b321920502493a7346bb2
aguiquiss03 <neinaldoaguinaldo13@gmail.com>
2024-07-11 17:36:45 +0100

    landing events

--- src/views/landing/Events.tsx
+++ src/views/landing/Events.tsx
@@ -1,7 +1,11 @@
+import BannerBaw from "../../containers/LandingPage/BannerBaws"
+import AtividadesRecentes from "../../containers/InfoPageEvent/AtividadesRecentes"
+
 const Events: React.FC = () => {
 	return (
-		<main>
-			<div>Etelvino</div>
+		<main className="bg-[#001032]">
+			<BannerBaw/>
+			<AtividadesRecentes/>
 		</main>
 	)
 }
