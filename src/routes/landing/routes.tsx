e996564aab3884837fd1c69605e2ca35128b4c09
joelsilva333 <joelpitra44@gmail.com>
2024-07-17 16:19:29 +0100

    Mudando rotas

--- src/routes/landing/routes.tsx
+++ src/routes/landing/routes.tsx
@@ -1,48 +1,16 @@
-import EnterpriseForm from "../../views/adesion/EnterpriseForm"
-import PersonalForm from "../../views/adesion/PersonalForm"
+
 import { RouteType } from "../../services/config/types"
-import InfoPage from "../../views/landing/Baw2024"
-import Events from "../../views/landing/Events"
-import Academy from "../../views/landing/Academy"
-import MesaRedonda from "../../views/landing/MesaRedonda"
+
+import HomePage from "../../views/landing/Home"
 
 const Routes: RouteType[] = [
 	{
 		label: "",
 		path: "/",
-		element: <MesaRedonda />,
+		element: <HomePage />,
 		type: "public",
 	},
-	{
-		label: "",
-		path: "/events",
-		element: <Events />,
-		type: "public",
-	},
-	{
-		label: "",
-		path: "/academy",
-		element: <Academy />,
-		type: "public",
-	},
-	{
-		label: "",
-		path: "/landing-events",
-		element: <InfoPage />,
-		type: "public",
-	},
-	{
-		label: "",
-		path: "/enterprise",
-		element: <EnterpriseForm />,
-		type: "adesion",
-	},
-	{
-		label: "",
-		path: "/personal",
-		element: <PersonalForm />,
-		type: "adesion",
-	},
+	
 ]
 
 export const PublicRoutes = Routes
