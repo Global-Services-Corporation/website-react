5f826519346874a02d280ba1fd65ce63ccd6c573
joelsilva333 <joelpitra44@gmail.com>
2024-08-08 15:59:21 +0100

    Colocando o Swiper na seção de oradores

--- src/containers/MesaRedonda/Speakers.tsx
+++ src/containers/MesaRedonda/Speakers.tsx
@@ -41,17 +41,40 @@ const Speakers: React.FC = () => {
 				<p className="font-extrabold text-3xl text-[#0E1937] text-center max-lg:text-2xl">
 					Speakers do Evento
 				</p>
-				<div className="flex pb-4">
-					<div className="w-full h-full items-center justify-center grid grid-cols-4 gap-10 max-lg:grid-cols-1">
+				<div className="flex pb-4"></div>
+
+				<div className="w-4/5 max-lg:w-11/12 flex justify-center items-center gap-2 h-fit text-black">
+					<Swiper
+						watchSlidesProgress={true}
+						slidesPerView={3}
+						spaceBetween={30}
+						autoplay={{
+							delay: 2500,
+							disableOnInteraction: false,
+						}}
+						breakpoints={{
+							0: {
+								slidesPerView: 1,
+							},
+							640: {
+								slidesPerView: 2,
+							},
+							1024: {
+								slidesPerView: 3,
+							},
+						}}
+						className="productsSwiper w-full"
+					>
 						{cardInfo.map((info, index) => (
-							<img
-								key={index}
-								src={info.imagem}
-								alt=""
-								className="object-contain hover:-translate-y-3 transition-all duration-700 hover:shadow-2xl shadow-md max-lg:w-80"
-							/>
+							<SwiperSlide key={index}>
+								<img
+									src={info.imagem}
+									alt=""
+									className="object-contain hover:-translate-y-3 transition-all duration-700 hover:shadow-2xl shadow-md max-lg:w-80"
+								/>
+							</SwiperSlide>
 						))}
-					</div>
+					</Swiper>
 				</div>
 			</div>
 		</section>
